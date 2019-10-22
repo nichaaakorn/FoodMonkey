c8f0f364223e1be54241062c14b29c82e1522719
Prae_Nicha <s6030213039@phuket.psu.ac.th>
2019-10-22 22:45:37 +0700

    update

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.addEventListener('init', function (event) {
     console.log("homePage");
 
     $("#menubtn").click(function () {
-      $("#sidemenu")[0].open();      
+      $("#sidemenu")[0].open();
     });
   }
 
@@ -14,69 +14,75 @@ document.addEventListener('init', function (event) {
     console.log("menuPage");
 
     $("#login").click(function () {
-      $("#content")[0].load("login.html");  
-      $("#sidemenu")[0].close();   
+      $("#content")[0].load("login.html");
+      $("#sidemenu")[0].close();
     });
 
     $("#home").click(function () {
-      $("#content")[0].load("home.html");  
-      $("#sidemenu")[0].close();   
+      $("#content")[0].load("home.html");
+      $("#sidemenu")[0].close();
     });
 
     $("#sweets").click(function () {
       console.log("sweets");
-      
-      $("#myNavigator")[0].pushPage("Sweet.html");   
+
+      $("#myNavigator")[0].pushPage("Sweet.html");
     });
 
     $("#street").click(function () {
       console.log(";;;;;;;");
-      
-      $("#myNavigator")[0].pushPage("Street.html");   
+
+      $("#myNavigator")[0].pushPage("Street.html");
     });
 
     $("#drink").click(function () {
       console.log(";;;;;;;");
-      
-      $("#myNavigator")[0].pushPage("drink.html");   
+
+      $("#myNavigator")[0].pushPage("drink.html");
     });
 
-    
+
     $("#islam").click(function () {
       console.log(";;;;;;;");
-      
-      $("#myNavigator")[0].pushPage("Islam.html");   
+
+      $("#myNavigator")[0].pushPage("Islam.html");
     });
 
-    
- 
 
-  
+
+
+
 
   }
   if (page.id === 'mypage') {
     console.log("mypage");
 
-   $("#menuSweet").click(function () {
+    $("#menuSweet").click(function () {
       console.log(";;;;;;;");
-      
-      $("#myNavigator")[0].pushPage("MenuSweet.html");   
+
+      $("#myNavigator")[0].pushPage("MenuSweet.html");
     });
 
-  
-   $("#menuStreet").click(function () {
-        console.log(";;;;;;;");
-        
-      $("#myNavigator")[0].pushPage("MenuStreet.html");   
+
+    $("#menuStreet").click(function () {
+      console.log(";;;;;;;");
+
+      $("#myNavigator")[0].pushPage("MenuStreet.html");
     });
-  
-      }
+
+    $("#menuDrink").click(function () {
+      console.log(";;;;;;;");
+
+      $("#myNavigator")[0].pushPage("MenuDrink.html");
+    });
+
+  }
 
   if (page.id === 'loginPage') {
     console.log("loginPage");
 
     $("#backhomebtn").click(function () {
-      $("#content")[0].load("home.html");      
+      $("#content")[0].load("home.html");
     });
   }
 });
